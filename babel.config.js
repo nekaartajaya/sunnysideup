8e60a69602bd32cf9508c90b7504954d797d2bb5
nekaartajaya <nekaartajaya@gmail.com>
2022-12-16 23:32:42 +0800

    feat: setup custom path

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            tests: ["./tests/"],
+            "@api": "./src/api",
+            "@components": "./src/components",
+            "@pages": "./src/pages",
+          },
+        },
+      ],
+    ],
   };
 };
